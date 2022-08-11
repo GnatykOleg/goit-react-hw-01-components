da1082220d350607688cfcc5408c252f5d1d3e6d
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2022-08-11 18:42:07 +0300

    Add re export js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Profile from './Profile/Profile';
-import Statistics from './Statistics/Statistics';
-import FriendList from './FriendList/FriendList';
-import TransactionHistory from './Transactions/TransactionHistory';
+// import Profile from './Profile/Profile';
+// import Statistics from './Statistics/Statistics';
+// import FriendList from './FriendList/FriendList';
+// import TransactionHistory from './Transactions/TransactionHistory';
+import {
+  Profile,
+  Statistics,
+  FriendList,
+  TransactionHistory,
+} from './components';
 
-import user from '../data//user';
-import data from '../data/data';
-import friends from '../data/friends';
-import transactions from '../data/transactions';
+import user from './data/user';
+import data from './data/data';
+import friends from './data/friends';
+import transactions from './data/transactions';
 
 export default function App() {
   return (
